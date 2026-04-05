c252cfee62d781034be17ac89875d6781ec61f4c
renanvilelati <renanvilelati@gmail.com>
2026-04-05 18:25:17 -0300

    feat: add CreateUserUseCase

---
+++ src/use-cases/create-user.js
@@ -0,0 +1,29 @@
+import { v4 as uuidv4 } from 'uuid';
+import bcrypt from 'bcrypt';
+import { PostgresCreateUserRepository } from '../repositories/postgres/create-user';
+
+export class CreateUserUseCase {
+  async execute(createUserParams) {
+    // Verificar se existe usuário cadastrado no banco
+
+    // Gerar ID do usuário
+    const userId = uuidv4();
+
+    // Criptografat a senha
+    const hashedPassword = bcrypt.hash(createUserParams.password, 10);
+
+    // Inserir o usuário no banco de dados
+    const user = {
+      ...createUserParams,
+      id: userId,
+      password: hashedPassword,
+    };
+
+    // Chamar repositório
+    const postgresCreateUserRepository = new PostgresCreateUserRepository();
+
+    const createdUser = await postgresCreateUserRepository.execute(user);
+
+    return createdUser;
+  }
+}
