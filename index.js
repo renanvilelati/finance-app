6219e4c2010faf4ab21e775c7c7c60638a0f2a6d
renanvilelati <renanvilelati@gmail.com>
2026-04-06 14:34:26 -0300

    feat: add get user by id endpoint

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 import 'dotenv/config.js';
 import express, { json } from 'express';
 import { CreateUserController } from './src/controllers/create-user.js';
+import { GetUserByIdController } from './src/controllers/get-user-by-id.js';
 
 const app = express();
 app.use(json());
@@ -11,6 +12,17 @@ app.post('/api/users', async (request, response) => {
   const createUserController = new CreateUserController();
 
   const { status, data, message } = await createUserController.execute(request);
+
+  response.status(status).json({
+    data,
+    message,
+  });
+});
+
+app.get('/api/users/:userId', async (request, response) => {
+  const getUserController = new GetUserByIdController();
+
+  const { status, data, message } = await getUserController.execute(request);
 
   response.status(status).json({
     data,
