99ee2e5082a5653b61562cc9a02047cde4cf619d
renanvilelati <renanvilelati@gmail.com>
2026-04-06 01:25:27 -0300

    feat: add create user use-case

--- src/use-cases/create-user.js
+++ src/use-cases/create-user.js
@@ -1,6 +1,6 @@
 import { v4 as uuidv4 } from 'uuid';
 import bcrypt from 'bcrypt';
-import { PostgresCreateUserRepository } from '../repositories/postgres/create-user';
+import { PostgresCreateUserRepository } from '../repositories/postgres/create-user.js';
 
 export class CreateUserUseCase {
   async execute(createUserParams) {
@@ -10,7 +10,9 @@ export class CreateUserUseCase {
     const userId = uuidv4();
 
     // Criptografat a senha
-    const hashedPassword = bcrypt.hash(createUserParams.password, 10);
+    const hashedPassword = await bcrypt.hash(createUserParams.password, 10);
+
+    console.log('hashedPassword', hashedPassword);
 
     // Inserir o usuário no banco de dados
     const user = {
